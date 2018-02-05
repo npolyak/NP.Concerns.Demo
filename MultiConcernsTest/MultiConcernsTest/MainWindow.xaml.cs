561d6aabdee9ef1c53be9892df6fda4328b94248
npolyak <nick.polyak@verizon.net>
2018-02-05 16:41:36 -0500

    removed unnecessary test vm

--- MultiConcernsTest/MultiConcernsTest/MainWindow.xaml.cs
+++ MultiConcernsTest/MultiConcernsTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultiConcernsTest.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,31 @@ namespace MultiConcernsTest
         public MainWindow()
         {
             InitializeComponent();
+
+            BusinessGroupsVM businessGroups = new BusinessGroupsVM();
+
+            // build some test data
+            BusinessGroupVM businessGroup1 = new BusinessGroupVM
+            {
+                Name = "Astrologists"
+            };
+
+            businessGroup1.People.Add(new PersonVM { FirstName = "Joe", LastName = "Doe" });
+            businessGroup1.People.Add(new PersonVM { FirstName = "Jane", LastName = "Dane" });
+
+            businessGroups.Add(businessGroup1);
+
+            BusinessGroupVM businessGroup2 = new BusinessGroupVM
+            {
+                Name = "Alchemists"
+            };
+
+            businessGroup2.People.Add(new PersonVM { FirstName = "Michael", LastName = "Mont" });
+            businessGroup2.People.Add(new PersonVM { FirstName = "Michelle", LastName = "Mitchell" });
+
+            businessGroups.Add(businessGroup2);
+
+            this.DataContext = businessGroups;
         }
     }
 }
