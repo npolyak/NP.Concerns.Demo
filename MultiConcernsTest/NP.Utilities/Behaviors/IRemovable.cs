5d9fd75d2451e17d86698993671c89623d47e160
npolyak <nick.polyak@verizon.net>
2018-02-05 15:38:37 -0500

    added NP.Utilities to source control

---
+++ MultiConcernsTest/NP.Utilities/Behaviors/IRemovable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Utilities.Behaviors
+{
+    public interface IRemovable
+    {
+        event Action<IRemovable> RemoveEvent;
+
+        void Remove();
+    }
+}
---
+++ MultiConcernsTest/NP.Utilities/Behaviors/Removable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Utilities.Behaviors
+{
+    public class Removable : IRemovable
+    {
+        public event Action<IRemovable> RemoveEvent;
+
+        public void Remove()
+        {
+            RemoveEvent?.Invoke(this);
+        }
+    }
+}
