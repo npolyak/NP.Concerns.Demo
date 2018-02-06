0430a96b69ed397ed68c1c7c27ee8d69896ad65f
npolyak <nick.polyak@verizon.net>
2018-02-06 16:24:22 -0500

    added documentation.

--- MultiConcernsRoxyTest/MainWindow.xaml.cs
+++ MultiConcernsRoxyTest/MainWindow.xaml.cs
@@ -14,43 +14,56 @@ namespace MultiConcernsRoxyTest
         {
             InitializeComponent();
 
+            // create the generated types
             RoxyModelAssembler.AssembleSelectableRemovablePerson();
-
             RoxyModelAssembler.AssembleSelectableRemovableBusinessGroup();
-
             RoxyModelAssembler.AssembleBusinessGroupsCollection();
 
+            // get the data context as BusinessGroupsVM type
             SingleSelectionObservableCollection<ISelectableRemovableBusinessGroup> dataContext =
                 Core.GetInstanceOfGeneratedType<SingleSelectionObservableCollection<ISelectableRemovableBusinessGroup>>();
 
+            // set the data context of the main window
             this.DataContext = dataContext;
 
+           // create businessGroup1 as BusingGroupVM object
            ISelectableRemovableBusinessGroup businessGroup1 = 
                 Core.GetInstanceOfGeneratedType<ISelectableRemovableBusinessGroup>();
 
             businessGroup1.Name = "Astrologists";
+
+            // add businessGroup1 to the data context collection
             dataContext.Add(businessGroup1);
 
+            // create person1 as PersonVM object
             ISelectableRemovablePerson person1 = Core.GetInstanceOfGeneratedType<ISelectableRemovablePerson>();
+
+            //set properties
             person1.FirstName = "Joe";
             person1.LastName = "Doe";
+
+            // add person1 object to businessGroup1
             businessGroup1.People.Add(person1);
 
+            // create and add person2
             ISelectableRemovablePerson person2 = Core.GetInstanceOfGeneratedType<ISelectableRemovablePerson>();
             person2.FirstName = "Jane";
             person2.LastName = "Dane";
             businessGroup1.People.Add(person2);
 
+            // create and add businessGroup2
             ISelectableRemovableBusinessGroup businessGroup2 =
                 Core.GetInstanceOfGeneratedType<ISelectableRemovableBusinessGroup>();
             businessGroup2.Name = "Alchemists";
             dataContext.Add(businessGroup2);
 
+            // create and add person3 
             ISelectableRemovablePerson person3 = Core.GetInstanceOfGeneratedType<ISelectableRemovablePerson>();
             person3.FirstName = "Michael";
             person3.LastName = "Mont";
             businessGroup2.People.Add(person3);
 
+            // create and add person4
             ISelectableRemovablePerson person4 = Core.GetInstanceOfGeneratedType<ISelectableRemovablePerson>();
             person4.FirstName = "Michelle";
             person4.LastName = "Mitchell";
--- MultiConcernsRoxyTest/RoxyViewModels/RoxyModelAssembler.cs
+++ MultiConcernsRoxyTest/RoxyViewModels/RoxyModelAssembler.cs
@@ -12,11 +12,17 @@ using System.Threading.Tasks;
 
 namespace MultiConcernsRoxyTest.RoxyViewModels
 {
-    public interface ISelectableRemovableItem<T> : ISelectableItem<T>, IRemovable, INotifyPropertyChanged
+    // represents item that is selectable and removable
+    public interface ISelectableRemovableItem<T> : 
+        ISelectableItem<T>, 
+        IRemovable, 
+        INotifyPropertyChanged
        where T : ISelectableItem<T>
     {
     }
 
+    // Wrapper interface for implementing 
+    // ISelectableRemovableItem interface
     public interface ISelectableRemovableWrapper<T>
         where T : ISelectableItem<T>
     {
@@ -25,17 +31,27 @@ namespace MultiConcernsRoxyTest.RoxyViewModels
         Removable Removable { get; }
     }
 
+
+    // represents PersonVM - 
+    // the IPersonDataVM with Selectable and
+    // Removable functionality
     public interface ISelectableRemovablePerson :
         IPersonDataVM, ISelectableRemovableItem<ISelectableRemovablePerson>
     {
 
     }
 
+
+    // the wrapper for 
+    // implementing ISelectableRemovablePerson
     public interface ISelectableRemovablePersonWrapper :
         ISelectableRemovableWrapper<ISelectableRemovablePerson>
     {
     }
 
+
+    // represents IBusinessGroup with Selectable and
+    // Removable functionality
     public interface ISelectableRemovableBusinessGroup :
         IBusinessGroup,
         ISelectableRemovableItem<ISelectableRemovableBusinessGroup>
@@ -43,11 +59,16 @@ namespace MultiConcernsRoxyTest.RoxyViewModels
 
     }
 
+
+    // Wrapper for the RemovableCollectionBehavior
     public interface IRemovableCollectionBehaviorWrapper
     {
         RemovableCollectionBehavior TheRemovableCollectionBehavior { get; }
     }
 
+
+    // Wrapper for RemovableCollectionBehavior and 
+    // ParentChildSelectionBehavior
     public interface ISelectableRemovableBusinessGroupWrapper :
         ISelectableRemovableWrapper<ISelectableRemovableBusinessGroup>,
         IRemovableCollectionBehaviorWrapper
@@ -57,31 +78,50 @@ namespace MultiConcernsRoxyTest.RoxyViewModels
 
     public static class RoxyModelAssembler
     {
+
+        // assembles the functionality for PersonVM with 
+        // selectable and removable capabilities
         public static void AssembleSelectableRemovablePerson()
         {
+            // create type config
             ITypeConfig typeConfig =
-                Core.FindOrCreateTypeConfig<ISelectableRemovablePerson, PersonDataVM, ISelectableRemovablePersonWrapper>();
+                Core.FindOrCreateTypeConfig<ISelectableRemovablePerson, PersonDataVM, ISelectableRemovablePersonWrapper>("PersonVM");
 
+            // set the first argument for PropertyChanged event 
+            // to map into 'this'
             typeConfig.SetEventArgThisIdx(nameof(INotifyPropertyChanged.PropertyChanged), 0);
 
+            // complete the configuration
             typeConfig.ConfigurationCompleted();
         }
 
+        // Assembles the BusinessGroupVM functionality with 
+        // Selectable and Removable implementations and also 
+        // with ParentChildSelectionBehavior and RemovableCollectionBehavior.
         public static void AssembleSelectableRemovableBusinessGroup()
         {
+            // get the type config object
             ITypeConfig typeConfig =
-                Core.FindOrCreateTypeConfig<ISelectableRemovableBusinessGroup, ISelectableRemovableBusinessGroupWrapper>();
+                Core.FindOrCreateTypeConfig<ISelectableRemovableBusinessGroup, ISelectableRemovableBusinessGroupWrapper>("BusinessGroupVM");
 
+            // Adds the initialization of People collection to
+            // an empty SingleSelectionObservableCollection collection within the constructor
             typeConfig.SetInit<SingleSelectionObservableCollection<ISelectableRemovablePerson>>(nameof(IBusinessGroup.People));
 
+            // set the first argument for PropertyChanged event 
+            // to map into 'this'
             typeConfig.SetEventArgThisIdx(nameof(INotifyPropertyChanged.PropertyChanged), 0);
 
+            // maps Parent property of wrapped ParentChildSelectionBehavior
+            // into 'this' field of the generated type. 
             typeConfig.SetThisMemberMap
             (
                 nameof(ISelectableRemovableBusinessGroupWrapper.TheParentChildSelectionBehavior),
                 nameof(ParentChildSelectionBehavior<ISelectableRemovableBusinessGroup, ISelectableRemovablePerson>.Parent)
             );
 
+            // maps Children property of wrapped ParentChildCollectionBehavior
+            // into People property of the generated type. 
             typeConfig.SetMemberMap
             (
                 nameof(ISelectableRemovableBusinessGroupWrapper.TheParentChildSelectionBehavior),
@@ -89,6 +129,9 @@ namespace MultiConcernsRoxyTest.RoxyViewModels
                 nameof(IBusinessGroup.People)
             );
 
+
+            // maps TheCollection property of wrapped RemovableCollectionBehavior
+            // into People property of the generated type. 
             typeConfig.SetMemberMap
             (
                 nameof(ISelectableRemovableBusinessGroupWrapper.TheRemovableCollectionBehavior),
@@ -96,14 +139,18 @@ namespace MultiConcernsRoxyTest.RoxyViewModels
                 nameof(IBusinessGroup.People)
             );
 
+            // specifies that the configuration is completed.
             typeConfig.ConfigurationCompleted();
         }
 
+        // Assembles the BusinessGroupsVM collection. 
         public static void AssembleBusinessGroupsCollection()
         {
             ITypeConfig typeConfig =
-                Core.FindOrCreateTypeConfig<NoInterface, SingleSelectionObservableCollection<ISelectableRemovableBusinessGroup>, IRemovableCollectionBehaviorWrapper>();
+                Core.FindOrCreateTypeConfig<NoInterface, SingleSelectionObservableCollection<ISelectableRemovableBusinessGroup>, IRemovableCollectionBehaviorWrapper>("BusinessGroupsVM");
 
+            // maps TheCollection property of RemovableCollectionBehavior
+            // into 'this' of the generated object. 
             typeConfig.SetThisMemberMap
             (
                 nameof(IRemovableCollectionBehaviorWrapper.TheRemovableCollectionBehavior),
